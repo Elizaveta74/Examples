fb5890da1546e11cb867e4dcbf8aa830b850e55c
Elizaveta <lizasid@mail.ru>
2022-08-04 13:29:26 +0300

    Example 005 ConditionIFElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "liza")
+{
+    Console.WriteLine("Ура, это же LIZA!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
